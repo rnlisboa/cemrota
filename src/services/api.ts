d847a98534b91faec7995cfbfc699d8b59f4fe7a
rnlisboa <dev.renanlisboa@gmail.com>
2024-02-19 13:38:43 -0300

    feat: Configurando api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,32 @@
-import axios from "axios";
-
-export const api = axios.create({
-    baseURL: ''
-})
+import axios, { AxiosError, } from "axios";
+import { parseCookies } from "nookies";
+import { AuthTokenError } from "./errors/authTokenError";
 
 
-api.interceptors.response.use((config)=>{
-    
-})
+export function setupAPIClient(context =undefined){
+    let cookies = parseCookies(context);
+    const api = axios.create({
+        baseURL: 'http://localhost:3333',
+        headers: {
+            Authorization: `Bearer ${cookies['@cemrota.token']}`
+        }
+    })
+
+
+    api.interceptors.response.use(response => {
+        return response;
+        },(error: AxiosError) => {
+            if(error.response?.status === 401){
+                //deslogar
+                if(typeof window !== undefined){
+                    //deslogar
+                } else {
+                    return Promise.reject( new AuthTokenError);
+                }
+            }
+
+            return Promise.reject(error);
+        })
+
+    return api;
+}
